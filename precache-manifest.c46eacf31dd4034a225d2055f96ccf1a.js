a8fe1d69045a80e6dad83220d7c1153faf8b963d
Cuong Nguyen <huycuong10081996@gmail.com>
2019-11-12 23:36:11 +0700

    Updates

---
+++ precache-manifest.c46eacf31dd4034a225d2055f96ccf1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a211220af95765395b92793f175d98",
+    "url": "/portolifo/index.html"
+  },
+  {
+    "revision": "5d09379c0ed2785bd38e",
+    "url": "/portolifo/static/css/main.01f2330c.chunk.css"
+  },
+  {
+    "revision": "49c21045aff3458f1eb2",
+    "url": "/portolifo/static/js/2.ab180245.chunk.js"
+  },
+  {
+    "revision": "5d09379c0ed2785bd38e",
+    "url": "/portolifo/static/js/main.fa12932b.chunk.js"
+  },
+  {
+    "revision": "2c32d1f596ce3d088f0d",
+    "url": "/portolifo/static/js/runtime-main.fb22b0d6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portolifo/static/media/logo.25bf045c.svg"
+  }
+]);
